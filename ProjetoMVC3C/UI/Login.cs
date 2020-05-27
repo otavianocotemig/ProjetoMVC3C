142b82af5c73b3305e23d70b2851ed86797382b8
otavianocotemig <otaviano@cotemig.com.br>
2020-05-27 14:36:11 -0300

    Criação das Pastas do Projeto

--- ProjetoMVC3C/UI/Login.cs
+++ ProjetoMVC3C/UI/Login.cs
@@ -16,5 +16,10 @@ namespace ProjetoMVC3C
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
