326dc89752c605784b2703889139cfc3c0689e82
otavianocotemig <otaviano@cotemig.com.br>
2020-06-03 14:40:17 -0300

    Criacao da Classe DAL

---
+++ ProjetoMVC3C/DAL/DALMysql.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoMVC3C.DAL
+{
+    class DALMysql
+    {
+        private MySqlConnection conexao;
+        private string string_conexao = "Persist security info = false; " +
+                                        "server = localhost; " +
+                                        "database = dbmvc; " +
+                                        "user = root ; pwd=;";
+        public void conectar()
+        {
+            conexao = new MySqlConnection(string_conexao);
+            conexao.Open();
+        }
+
+
+
+    }
+}
